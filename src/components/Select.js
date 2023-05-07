a0ff158485b754e3c0f84254e3d489642be2449d
Christian Gleitze <christian-gleitze@web.de>
2023-05-08 00:09:50 +0200

    add components

---
+++ src/components/Select.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Select(props) {
+
+    /* 
+    * props:
+    * - produkte: Produktdaten
+    * - formular: Formularwerte
+    * - aendereKategorie: Funktion
+    */
+
+    return (
+        <>
+            <h2>Willkommen zum Shop!</h2>
+            <form>
+				<select value={props.formular.hauptkategorie} name="hauptkategorie"
+                    onChange={props.aendereKategorie}>
+					<option value="-1">Bitte Bereich wählen</option>
+                    {
+                        props.produkte.map((ele, index) => {
+                            /* ele = Objekt (Hauptkategorie) */
+                            return <option value={index}>{ele.name}</option>
+                        })
+                    }
+				</select>
+				{
+                    props.formular.hauptkategorie != "-1" &&
+                    <select value={props.formular.unterkategorie} name="unterkategorie"
+                        onChange={props.aendereKategorie}>
+                        <option value="-1">Bitte wählen</option>
+                        {
+                            props.produkte[props.formular.hauptkategorie].gruppe.map((ele, index) => {
+                                /* ele = Objekt (Unterkategorie) */
+                                return <option value={index}>{ele.name}</option>
+                            })
+                        }
+                    </select>
+                }
+            </form>
+        </>
+    );
+}
+
+export default Select;
